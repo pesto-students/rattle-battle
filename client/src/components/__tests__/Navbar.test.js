52e3a7c130aa64b7df3075edfaf3af20887113d0
Nitin Patel <nitinpatel278@gmail.com>
2019-02-15 03:20:11 +0530

    Move Enzyme Adapter configuration to setupTests.js

--- client/src/components/__tests__/Navbar.test.js
+++ client/src/components/__tests__/Navbar.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Enzyme, { mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { mount } from 'enzyme';
 
 import Navbar from '../Navbar';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Navbar />', () => {
   const title = 'Rattle Battle';
