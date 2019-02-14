d2f1082a34ce92ac2e63aaad3999ef66e8b9b00f
Nitin Patel <nitinpatel278@gmail.com>
2019-02-15 03:20:11 +0530

    Move Enzyme Adapter configuration to setupTests.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import App from './App';
 
-Enzyme.configure({ adapter: new Adapter() });
 it('renders without crashing', () => {
   shallow(<App />);
 });
