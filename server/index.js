98db10b9827443c0cc09dc841b3f50962e346936
Nitin Patel <nitinpatel278@gmail.com>
2019-02-19 02:36:38 +0530

    test: Add tests for index.js
    Resolves: #25

--- server/index.js
+++ server/index.js
@@ -4,7 +4,13 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.listen(PORT, () => {
+app.get('/ping', (req, res) => {
+  res.json({ status: 'working' });
+});
+
+const server = app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server Listening on port ${PORT}`);
 });
+
+module.exports = server;
