cad22f6a185e8704e6e707cd620e55c0e8a8db0a
Nitin Patel <nitinpatel278@gmail.com>
2019-02-19 02:36:38 +0530

    test: Add tests for index.js
    Resolves: #25

---
+++ server/__tests__/index.test.js
@@ -0,0 +1,42 @@
+const request = require('supertest');
+const server = require('../index');
+
+describe('Server responds correctly to requests', () => {
+  let testServer;
+
+  beforeEach(() => {
+    testServer = server;
+  });
+
+  afterEach(() => {
+    testServer.close();
+  });
+
+  test('should respond to GET `/ping`', (done) => {
+    request(testServer)
+      .get('/ping')
+      .then((response) => {
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ status: 'working' });
+        done();
+      });
+  });
+
+  test('should respond with 404 for unknown routes', async (done) => {
+    const randomRoute = () => `/abc${(Math.random() * 10000).toFixed(0)}`;
+
+    await request(testServer)
+      .get(randomRoute())
+      .expect(404);
+
+    await request(testServer)
+      .post(randomRoute())
+      .expect(404);
+
+    await request(testServer)
+      .put(randomRoute())
+      .expect(404);
+
+    done();
+  });
+});
