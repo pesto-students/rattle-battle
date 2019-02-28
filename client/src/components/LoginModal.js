16847b0c83fff89099efd56bc77a33e3458039ef
Nitin Patel <nitinpatel278@gmail.com>
2019-03-01 02:20:07 +0530

    feat: Attach server API to login form

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
 import FormDialog from './FormDialog';
 import CheckBoxWithLabel from './CheckBoxWithLabel';
 import useLoginForm from '../customHooks/useLoginForm';
 import RequiredFormTextField from './RequiredFormTextField';
+import UserContext from '../utils/user-context';
 
 const LoginModal = ({ closeModal, show }) => {
+  const { login: loginSuccessCallback } = useContext(UserContext);
   const {
-    emailState, passwordState, rememberMeState, handleSubmit,
-  } = useLoginForm(closeModal);
+    emailState, passwordState, rememberMeState, handleSubmit, formError,
+  } = useLoginForm(
+    loginSuccessCallback,
+  );
 
   return (
     <FormDialog title="Login" show={show} closeModal={closeModal} handleSubmit={handleSubmit}>
+      <Typography color="error" data-test="form-error">
+        {formError}
+      </Typography>
       <RequiredFormTextField name="email" type="email" label="Email" autoFocus {...emailState} />
       <RequiredFormTextField name="password" type="password" label="Password" {...passwordState} />
       <CheckBoxWithLabel label="Remember Me" name="remember-me" {...rememberMeState} />
