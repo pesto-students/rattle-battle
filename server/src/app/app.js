88c4674b934f21706fb1dc3faf116facd1390136
Nitin Patel <nitinpatel278@gmail.com>
2019-03-09 02:55:41 +0530

    aa

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -2,11 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import router from '../routes/routes';
-import { TRUSTED_ORIGIN } from './appConstants';
 
 const app = express();
 
-app.use(cors({ origin: TRUSTED_ORIGIN, exposedHeaders: ['X-Auth-Token'] }));
+app.use(cors({ origin: '*', exposedHeaders: ['X-Auth-Token'] }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(router);
