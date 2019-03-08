d4bc071110cd476bbf2987fc779791965cb17d04
Nitin Patel <nitinpatel278@gmail.com>
2019-03-08 23:30:00 +0530

    chore: Deploy server

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -4,6 +4,12 @@ import mongoConfig from './mongoConfig';
 
 // TODO: handle the error, when database is not able to connect,--
 //  --send a mail to dev team, when database fails to get connect.
-mongoose.connect(DB_URL, mongoConfig);
+mongoose.connect(DB_URL, mongoConfig, (err) => {
+  if (err) {
+    throw err;
+  }
+  // eslint-disable-next-line no-console
+  console.log('Connected to MongoDB');
+});
 
 export default mongoose;
